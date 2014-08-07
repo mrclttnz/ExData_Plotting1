2066a81e85deaed99902ddacf3089e390b3b7faa
Marco Lattanzio <marco.lattanzio@gmail.com>
2014-08-07 23:28:08 +0200

    added code to download and unzip the data file

--- readDataset.R
+++ readDataset.R
@@ -1,6 +1,12 @@
 
 setwd("C:/projects/Coursera/Exploratory Data Analysis/Assignment/week1/work/ExData_Plotting1/")
 
+# download and unzip the data file
+fileUrl = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileUrl, destfile="./exdata-data-household_power_consumption.zip", method="auto")
+unzip(zipfile="./exdata-data-household_power_consumption.zip", exdir=".")
+
+
 library(sqldf)
 
 inFile = file("./household_power_consumption.txt")
